94bac5ac50c0fafda340bdb52e300ef0682c107c
Grazielle <grazielleconceicao@yahoo.com.br>
2024-05-27 19:05:23 -0300

    fix: middleware adjustment for login and register routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,11 +12,18 @@ export async function middleware(request: NextRequest) {
     if ([400, 401, 403, 500].includes(response.status)) {
         request.nextUrl.pathname = '/login';
         return NextResponse.redirect(request.nextUrl);
+    } else {
+        if (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/register')) {
+            if (response.status === 200) {
+                request.nextUrl.pathname = '/account';
+                return NextResponse.redirect(request.nextUrl);
+            }
+        }
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: '/account'
+    matcher: ['/account', '/login', '/register']
 }
